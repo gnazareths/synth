31252609752cf479d5e1ac8583704e967245a9d5
gnazareths <guilherme@minerva.kgi.edu>
2016-12-16 12:49:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mypackage',
+  packages = ['mypackage'],
+  description = 'A random test lib',
+  author = 'Guilherme Nazareth de Souza',
+  author_email = 'guilherme@minerva.kgi.edu',
+  url = 'https://github.com/gnazareths/synth',
+  download_url = 'https://github.com/gnazareths/synth/tarball/0.1', # I'll explain this in a second
+  keywords = ['social science', 'synth', 'synthetic control model', 'optimization',
+  			  'counterfactual', 'counter-factual'], 
+  classifiers = [],
+)
