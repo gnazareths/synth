cc23578133f90e3bac517e3a119e6afa5d821c8f
gnazareths <guilherme@minerva.kgi.edu>
2017-01-12 18:44:42 -0300

    Update __init__.py

--- synth/__init__.py
+++ synth/__init__.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
-import synth
+import dataprep
+import get_estimate
+import get_v_0
+import get_v_1
+import get_w
+import synth_tables
+import v_rss
+import w_constraint
+import w_rss
 
-__all__ = ['synth']
+__all__ = ['dataprep', 'get_estimate', 'get_v_0', 'get_v_1', 
+		   'get_w', 'synth_tables', 'v_rss', 'w_constraint',
+		   'w_rss']
