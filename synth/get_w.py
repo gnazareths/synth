5596b841ce780c6e1e9729529f4749009e6cdcb2
gnazareths <guilherme@minerva.kgi.edu>
2017-05-29 09:53:30 -0400

    Delete get_w.py

--- synth/get_w.py
+++
@@ -1,7 +0,0 @@
-from scipy.optimize import fmin_slsqp
-
-def get_w(w, v, x0, x1):
-    result = fmin_slsqp(w_rss, w, f_eqcons=w_constraint, bounds=[(0.0, 1.0)]*len(w),
-             args=(v, x0, x1), disp=False, full_output=True)
-    weights = result[0]
-    return weights
