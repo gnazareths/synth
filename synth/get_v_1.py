0bde27ec0161855e42bc9607db2e64be80ebdc0c
gnazareths <guilherme@minerva.kgi.edu>
2017-05-29 09:53:24 -0400

    Delete get_v_1.py

--- synth/get_v_1.py
+++
@@ -1,6 +0,0 @@
-from scipy.optimize import minimize
-
-def get_v_1(v, w, x0, x1, z0, z1):
-    result = minimize(get_v_0, v, args=(w, x0, x1, z0, z1), bounds=[(0.0, 1.0)]*len(v))
-    importance = result.x
-    return importance
